31bad64579f64b9678e3c43c24104cd9be98457d
«Rinat» <rtatarin949@gmail.com>
2022-09-30 10:36:07 +0300

    sem3_1

---
+++ seminar_3/3_1.py
@@ -0,0 +1,14 @@
+# 1. Задайте список, состоящий из произвольных чисел, количество задаёт пользователь.
+#    Напишите программу, которая определит, присутствует ли в заданном списке число,
+#    полученное от пользователя.
+
+from random import sample
+
+def num_in_list(count, number):
+    myList = sample(range(1,count * 2), count )
+    print(myList)
+    if number in myList:
+        return 'yes'
+    return 'no'
+
+print(num_in_list(int(input()), int(input())))
